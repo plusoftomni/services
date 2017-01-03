08807e598ee8919d28733a184d00d7ecd6ee8d45
victor <victorlima@plusoft.com.br>
2017-01-03 15:17:03 -0200

    geUrl

--- src/main/java/br/com/omni/services/sample/knowledgebase/PortalService.java
+++ src/main/java/br/com/omni/services/sample/knowledgebase/PortalService.java
@@ -2,6 +2,9 @@ package br.com.omni.services.sample.knowledgebase;
 
 import static br.com.omni.services.sample.RequestMethod.GET;
 import static br.com.omni.services.sample.ServiceHelper.encode;
+
+import java.util.Optional;
+
 import br.com.omni.services.sample.Functionality;
 import br.com.omni.services.sample.GenericService;
 import br.com.omni.services.sample.ServiceHelper;
@@ -13,29 +16,29 @@ public class PortalService extends GenericService {
 	private static final int DELETE_ID = 10;
 
 	public static void main(String[] args) throws Exception {
-		System.out.println("Requesting all");
-		requestAll();
+		//System.out.println("Requesting all");
+		//requestAll();
 		
-		System.out.format("\n\nRequesting by ID (%1$1s)\n",FIND_ID);
-		request(FIND_ID);
+		//System.out.format("\n\nRequesting by ID (%1$1s)\n",FIND_ID);
+		//request(FIND_ID);
 
-		System.out.format("\n\nRequesting by Name (%1$1s)\n",FIND_NAME);
-		request(FIND_NAME);
+		//System.out.format("\n\nRequesting by Name (%1$1s)\n",FIND_NAME);
+		//request(FIND_NAME);
 
-		System.out.println("\n\nRequesting 2 of 3");
-		request(2,2);
+		//System.out.println("\n\nRequesting 2 of 3");
+		//request(2,2);
 
-		System.out.println("\n\nRequesting all active");
-		request(false);
+		//System.out.println("\n\nRequesting all active");
+		//request(false);
 
-		System.out.println("\n\nRequesting all inactive");
-		request(true);
+		//System.out.println("\n\nRequesting all inactive");
+		//request(true);
 		
 		//System.out.println("\n\nCreating a new");
 		//create();
 
-		//System.out.println("\n\nUpdating");
-		//update(Optional.empty());
+		System.out.println("\n\nUpdating");
+		update(Optional.empty());
 
 		//System.out.format("\n\nDeleting (%1$1s)\n",DELETE_ID);
 		//delete(DELETE_ID);
